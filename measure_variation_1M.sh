b6a0c6e0001f0d2fda5620f5bec6413e92ecf8cf
user-jkj <s2331099uy@s.chibakoudai.jp>
2024-11-01 02:00:22 +0900

    Update measure_variation_1M.sh

--- measure_variation_1M.sh
+++ measure_variation_1M.sh
@@ -10,5 +10,5 @@ rm -f ${FILE}
 for i in $(seq 1000)
 do
     echo -n "${i} " >> ${FILE}
-    ab -c 10 -n 10000 ${URL} | grep 'Requests per second' | sed -r 's/.*( +)([0-9]+\.[0-9][0-9]) .*/\2/' >> ${FILE}
+    ab -c 10 -n 1000 ${URL} | grep 'Requests per second' | sed -r 's/.*( +)([0-9]+\.[0-9][0-9]) .*/\2/' >> ${FILE}
 done
